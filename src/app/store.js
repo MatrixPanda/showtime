15f7fe2a1be9af47c7359b559a39b85a0f8db885
MatrixPanda <zehan11s@hotmail.com>
2022-12-07 01:43:37 -0500

    quick api issue fix

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 // redux tool-kit store
 import { configureStore } from '@reduxjs/toolkit';
+
 import { tmdbApi } from '../services/TMDB';
 
 export default configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
 });
