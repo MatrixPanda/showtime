ed946bc76e49160db23f2921f76ae154e56d4b67
MatrixPanda <zehan11s@hotmail.com>
2022-12-09 00:42:23 -0500

    added redux slice

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { tmdbApi } from '../services/TMDB';
+import { genreOrCategory } from '../features/currentGenreOrCategory';
 
 export default configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
+    currentGenreOrCategory: genreOrCategory.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
 });
---
+++ src/features/currentGenreOrCategory.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// NOTE: redux slice is like a slice of cake
+// the entire react application state, or a store is the whole piece of cake
+// genreOrCategory is only one slice of that cake. Want to keep that info separate from movie or everything else
+// thats why we created genreOrCategory slice. Inside of this slice we can select a genre and do some logic
+// in this case, a reducer that selects a genre or category and updates it
+// then redux automatically gives us an action creator to be exported (=genreOrCategory.actions)
+// then connect that reducer to the store
+// then finally if we import useDispatch from react-redux in a component AND import selectGenreOrCategory
+// then we can use dispatch(selectGenreOrCategory(value)) to use the function's logic on that value
+export const genreOrCategory = createSlice({
+  name: 'genreOrCategory',
+  initialState: {
+    genreOrCategoryName: '',
+    page: 1,
+    searchQuery: '',
+  },
+  reducers: {
+    // created a function, export this function, then call it from component to populate genreOrCategoryName
+    selectGenreOrCategory: (state, action) => {
+      console.log(action.payload);
+      // state.genreOrCategoryName =
+    },
+  },
+});
+
+export const { selectGenreOrCategory } = genreOrCategory.actions;
+
+export default genreOrCategory.reducer; // Remember, whenever there is a reducer, I must attach it to the store
