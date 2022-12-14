3c82b061dcba2528ed9f90356709a72fb70b15fc
MatrixPanda <zehan11s@hotmail.com>
2022-12-14 17:42:46 -0500

    Added next steps notes to Actors.jsx

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// NOTE: steps to make it work
+// use useParams to get the actor's id
+// make a new call using redux toolkit query -> get actor details call...
+// research tmdb api docs for that particular api usage
+// use newly created useGetActorHook to get actor's info to the component
+
 const Actors = () => {
   console.log('Actors');
 
