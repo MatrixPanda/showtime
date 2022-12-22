c1814e95274f72e09cc4994d02f8d5d50d75a571
MatrixPanda <zehan11s@hotmail.com>
2022-12-22 11:33:04 -0500

    Added mroe features to Alan AI

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,18 +1,34 @@
-import React, { useEffect, useContext } from 'react';
+import { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-import { fetchToken } from '../utils';
+import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
 import { ColorModeContext } from '../utils/ToggleColorMode';
+import { fetchToken } from '../utils';
 
-// Alan is not going to be a component, its going to be a custom hook
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     alanBtn({
       key: 'acc641d72fde748908111ce2ea4806302e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: ({ command, mode }) => {
-        if (command === 'changeMode') {
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === 'chooseGenre') {
+          const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+
+          if (foundGenre) {
+            history.push('/');
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            // top rated has a space in it which will break it without the check below
+            const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+            history.push('/');
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === 'changeMode') {
           if (mode === 'light') {
             setMode('light');
           } else {
@@ -22,7 +38,9 @@ const useAlan = () => {
           fetchToken();
         } else if (command === 'logout') {
           localStorage.clear();
-          window.location.href = '/';
+          history.push('/');
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
         }
       },
     });
