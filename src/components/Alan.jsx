644afd0d82ea7fd447bfc295f99ebb2e99c55243
MatrixPanda <zehan11s@hotmail.com>
2022-12-22 06:09:08 -0500

    added alan AI

---
+++ src/components/Alan.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useContext } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import { ColorModeContext } from '../utils/ToggleColorMode';
+
+// Alan is not going to be a component, its going to be a custom hook
+const useAlan = () => {
+  const { setMode } = useContext(ColorModeContext);
+
+  useEffect(() => {
+    alanBtn({
+      key: 'acc641d72fde748908111ce2ea4806302e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, mode }) => {
+        if (command === 'changeMode') {
+          if (mode === 'light') {
+            setMode('light');
+          } else {
+            setMode('dark');
+          }
+        }
+      },
+    });
+  }, []);
+};
+
+export default useAlan;
